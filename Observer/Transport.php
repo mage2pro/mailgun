c321c34bfac92b91be4ceb260194bade5f2b67c2
Dmitry Fedyuk <dfediuk@gmail.com>
2018-01-28 02:12:16 +0600

    0.0.2

---
+++ Observer/Transport.php
@@ -0,0 +1,16 @@
+<?php
+namespace Dfe\Mailgun\Observer;
+use Magento\Framework\Event\Observer;
+use Magento\Framework\Event\ObserverInterface;
+// 2018-01-28
+final class Transport implements ObserverInterface {
+	/**
+	 * 2018-01-28
+	 * @override
+	 * @see ObserverInterface::execute()
+	 * @used-by \Magento\Framework\Event\Invoker\InvokerDefault::_callObserverMethod()
+	 * @param Observer $ob
+	 */
+	function execute(Observer $ob) {}
+}
+
