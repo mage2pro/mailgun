38e8701a7f3387ece14acb7da10bbd920d522668
Dmitry Fedyuk <dfediuk@gmail.com>
2018-01-28 06:09:08 +0600

    0.0.6

--- Transport.php
+++ Transport.php
@@ -1,23 +1,14 @@
 <?php
 namespace Dfe\Mailgun;
 use Magento\Framework\Exception\MailException;
-use Magento\Framework\Mail\MessageInterface as IMessage;
-use Magento\Framework\Mail\TransportInterface as ITransport;
 // 2018-01-28
 /** @final Unable to use the PHP «final» keyword here because of the M2 code generation. */
-class Transport implements ITransport {
+class Transport extends \Df\Framework\Mail\Transport {
 	/**
 	 * 2018-01-28
+	 * @final Unable to use the PHP «final» keyword here because of the M2 code generation.
 	 * @override
-	 * @see ITransport::getMessage()
-	 * @return IMessage
-	 */
-	function getMessage() {return null;}
-
-	/**
-	 * 2018-01-28
-	 * @override
-	 * @see ITransport::getMessage()
+	 * @see \Magento\Framework\Mail\TransportInterface::getMessage()
 	 * @return void
 	 * @throws MailException
 	 */
